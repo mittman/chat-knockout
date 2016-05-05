e0d29cd747f69ac6f539d4f27cfd0c016a3c7c55
mittman <mittman@users.noreply.github.com>
2016-05-04 22:42:49 -0700

    [CPSC 473-01] Assignment 9 (1/2)

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
+// Client-side code
+/* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, undef: true, unused: true, strict: true, trailing: true */
+/* global ko: true */
+
 var main = function () {
     "use strict";
 
-    var addCommentFromInputBox = function () {
-        var $new_comment;
+    function Chat(data) {
+       var self = this;
+       self.comment = ko.observable(data);
+    }
 
-        if ($(".comment-input input").val() !== "") {
-            $new_comment = $("<p>").text($(".comment-input input").val());
-            $new_comment.hide();
-            $(".comments").append($new_comment);
-            $new_comment.fadeIn();
-            $(".comment-input input").val("");
-        }
-    };
+    function ChatViewModel() {
+        var self = this;
+        self.feedback = ko.observableArray([]);
+        self.msg = ko.observable();
 
-    $(".comment-input button").on("click", function (event) {
-        addCommentFromInputBox();
-    });
+        self.addComment = function() {
+            self.feedback.push(new Chat(self.msg()));
+            self.msg("");
+        };
+    }
 
-    $(".comment-input input").on("keypress", function (event) {
-        if (event.keyCode === 13) {
-            addCommentFromInputBox();
-        }
-    });
+    ko.applyBindings(new ChatViewModel());
 };
 
 $(document).ready(main);
